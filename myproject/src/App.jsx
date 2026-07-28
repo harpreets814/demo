41996517231018003aa5c39acb5656fcaa681c6d
harpreets814 <hs6062880@gmail.com>
2026-07-28 15:30:01 +0530

    Todo App

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -2,24 +2,41 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0);
-    
+    const [todos, settodos] = useState([{
+      title:"go to gym",
+      description:"go to gym at 6 am",
+      isCompleted:false
+    },{
+      title:"go to school",
+      description:"go to school at 8 am",
+      isCompleted:false
+    }]);
+  function addTodo(){
+    settodos([...todos,{
+      title:"new todo",
+      description:"new todo description",
+      isCompleted:false
+    }])
+  }
     
   return (
     <div>
-      <CustomButton count={count} setcount={setCount}/> 
+      <button onClick={addTodo}>Add Todo</button>
+      {
+        todos.map(function(todo){
+          return<Todo title={todo.title} description={todo.description} isCompleted={todo.isCompleted}/>
+        })
+      }
     </div>
   )
 }
-function CustomButton(props){
-  function onclickhandler(){
-    props.setcount(props.count + 1);
-    
+function Todo({title,description,isCompleted}){
+  return <div>
+    <h1>{title}</h1>
+    <h2>{description}</h2>
+    <h3>{isCompleted?"Completed":"Not Completed"}</h3>
+  </div>
   }
-  return(
-    <button onClick={onclickhandler}>Counter{props.count}</button>
-  )
-}
-
+ 
 
 export default App
